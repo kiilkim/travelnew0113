a98f4c477ca86bb262103525b9a8b1e40c2c446b
kiilkim <58096214+kiilkim@users.noreply.github.com>
2020-01-14 17:37:17 +0900

    0114 change

---
+++ indexjs.js
@@ -0,0 +1,14 @@
+function main_next() {
+        
+    a=prompt("환영합니다. 아이디확인해주세요!")
+
+    if(isNaN(a)==true) {
+        alert(a+"님 환영합니다.");
+        location.href="bg.html";
+
+    }
+        
+
+    
+
+}
