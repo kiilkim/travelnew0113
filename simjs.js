e45dca9a508438bfe39d5c714face5633d554f07
kiilkim <58096214+kiilkim@users.noreply.github.com>
2020-01-15 17:40:10 +0900

    0115
    img plus, script ~ing

---
+++ simjs.js
@@ -0,0 +1,28 @@
+/*버튼 
+규칙
+1. html 파일에는 파일번호와 같은 버튼번호
+2. js파일에 다음으로 넘어가는 html은 파일넘버+1*/ 
+function bgsim_next() {
+    location.href="sim01.html"
+}
+
+function sim01_next() {
+    location.href="sim02.html"
+}
+
+function sim02_Townnext() {
+    location.href="sim03Town.html"
+}
+function sim02_Schoolnext() {
+    location.href="sim03School.html"
+}
+
+function sim03_next() {
+    location.href="sim04.html"
+}
+
+
+function bg12mouse() {
+    
+    
+}
